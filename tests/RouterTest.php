2d6b55087c001aaaa304e7a719d700b6ee3ded14
Nathan Bishop <nbish11@hotmail.com>
2016-06-22 21:54:13 +1000

    Closes #3

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -7,6 +7,7 @@ use Meek\Routing\Route\Collection;
 use Meek\Routing\Route\Matcher;
 use Meek\Routing\Route\Dispatcher;
 use Meek\Routing\Path\Generator;
+use Zend\Diactoros\ServerRequest;
 
 class RouterTest extends TestCase
 {
@@ -42,6 +43,52 @@ class RouterTest extends TestCase
         $this->assertInstanceOf(Generator::class, $this->router->getGenerator());
     }
 
+    public function testProxiesMatcher()
+    {
+        $route = new Route('test.route', '/', function () {});
+        $collection = new Collection([$route]);
+        $request = $this->getRequestWithPath('/');
+
+        $matcher = $this->createMock(Matcher::class);
+        $matcher->expects($this->once())
+            ->method('match')
+            ->with($request);
+
+        $router = new Router($collection, $matcher, new Dispatcher($matcher), new Generator($collection));
+        $router->match($request);
+    }
+
+    public function testProxiesDispatcher()
+    {
+        $route = new Route('test.route', '/', function () {});
+        $collection = new Collection([$route]);
+        $request = $this->getRequestWithPath('/');
+
+        $dispatcher = $this->createMock(Dispatcher::class);
+        $dispatcher->expects($this->once())
+            ->method('dispatch')
+            ->with($request);
+
+        $router = new Router($collection, new Matcher($collection), $dispatcher, new Generator($collection));
+        $router->dispatch($request);
+    }
+
+    public function testProxiesGenerator()
+    {
+        $route = new Route('test.route', '/:test', function () {});
+        $collection = new Collection([$route]);
+        $request = $this->getRequestWithPath('/');
+        $matcher = new Matcher($collection);
+
+        $generator = $this->createMock(Generator::class);
+        $generator->expects($this->once())
+            ->method('generate')
+            ->with('test.route', ['test' => 'tested']);
+
+        $router = new Router($collection, $matcher, new Dispatcher($matcher), $generator);
+        $router->generate('test.route', ['test' => 'tested']);
+    }
+
     public function testRouterProxiesUnknownMethodsToRouteCollection()
     {
         $route = new Route('test.route', '/', function () {});
@@ -53,5 +100,10 @@ class RouterTest extends TestCase
         $router = new Router($collection, $matcher, new Dispatcher($matcher), new Generator($collection));
 
         $this->assertSame($route, $router->add($route));
+    }
+
+    private function getRequestWithPath($path)
+    {
+        return new ServerRequest([], [], $path);
     }
 }
